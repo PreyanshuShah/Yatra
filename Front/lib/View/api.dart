523ca7385ab973e7b8b1b150b93e35e477da836b
Preyanshu Shah <165466380+PreyanshuShah@users.noreply.github.com>
2025-03-26 20:40:33 +0545

    security check

--- Front/lib/View/api.dart
+++ Front/lib/View/api.dart
@@ -1,6 +1,6 @@
-
 const String baseUrl = "http://127.0.0.1:8000/api/";
 
+// lo hai
 class APIEndpoints {
   static const String register = "${baseUrl}register/";
   static const String login = "${baseUrl}login/";
